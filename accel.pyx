12781b0a4eec16df9c0ac2baf571d312c46a50ad
obiwac <obiwac@gmail.com>
2023-12-24 22:05:56 +0100

    accel: Super aggressive optimization of `can_render_face`

--- accel.pyx
+++ accel.pyx
@@ -2,9 +2,9 @@
 
 import chunk
 
-CHUNK_WIDTH = 16
-CHUNK_HEIGHT = 128
-CHUNK_LENGTH = 16
+cdef int CHUNK_WIDTH = 16
+cdef int CHUNK_HEIGHT = 128
+cdef int CHUNK_LENGTH = 16
 
 def fast_get_block_number(chunks, position):
 	x, y, z = position
@@ -36,8 +36,20 @@ def fast_is_opaque_block(self, position):
 
 	return not block_type.transparent
 
-def can_render_face(chunks, block_types, block_number, block_type, position):
-	adj_number = fast_get_block_number(chunks, position)
+cdef can_render_face(parent_blocks, chunks, block_types, block_number, block_type, int x_, int y_, int z_):
+	cdef int x = x_ % CHUNK_WIDTH
+	cdef int y = y_ % CHUNK_HEIGHT
+	cdef int z = z_ % CHUNK_LENGTH
+
+	if x == 0 or x == CHUNK_WIDTH - 1 or y == 0 or y == CHUNK_HEIGHT - 1 or z == 0 or z == CHUNK_LENGTH - 1:
+		adj_number = fast_get_block_number(chunks, (x_, y_, z_))
+
+	else:
+		adj_number = parent_blocks[
+			x * CHUNK_LENGTH * CHUNK_HEIGHT +
+			z * CHUNK_HEIGHT +
+			y]
+
 	adj_type = block_types[adj_number]
 
 	if not adj_type or adj_type.transparent:
@@ -83,6 +95,9 @@ def fast_update_mesh(self):
 
 	chunks = self.world.chunks
 	block_types = self.world.block_types
+	parent_blocks = self.parent.blocks
+
+	cdef int x, y, z
 
 	for local_x in range(SUBCHUNK_WIDTH):
 		for local_y in range(SUBCHUNK_HEIGHT):
@@ -91,27 +106,29 @@ def fast_update_mesh(self):
 				parent_ly = self.local_position[1] + local_y
 				parent_lz = self.local_position[2] + local_z
 
-				block_number = self.parent.get_block(parent_lx, parent_ly, parent_lz)
+				block_number = self.parent.blocks[
+					parent_lx * CHUNK_LENGTH * CHUNK_HEIGHT +
+					parent_lz * CHUNK_HEIGHT +
+					parent_ly]
 
 				if block_number:
 					block_type = self.world.block_types[block_number]
 
-					x, y, z = (
-						self.position[0] + local_x,
-						self.position[1] + local_y,
-						self.position[2] + local_z)
+					x = self.position[0] + local_x
+					y = self.position[1] + local_y
+					z = self.position[2] + local_z
 
 					# if block is cube, we want it to check neighbouring blocks so that we don't uselessly render faces
 					# if block isn't a cube, we just want to render all faces, regardless of neighbouring blocks
 					# since the vast majority of blocks are probably anyway going to be cubes, this won't impact performance all that much; the amount of useless faces drawn is going to be minimal
 
 					if block_type.is_cube:
-						if can_render_face(chunks, block_types, block_number, block_type, (x + 1, y, z)): add_face(0)
-						if can_render_face(chunks, block_types, block_number, block_type, (x - 1, y, z)): add_face(1)
-						if can_render_face(chunks, block_types, block_number, block_type, (x, y + 1, z)): add_face(2)
-						if can_render_face(chunks, block_types, block_number, block_type, (x, y - 1, z)): add_face(3)
-						if can_render_face(chunks, block_types, block_number, block_type, (x, y, z + 1)): add_face(4)
-						if can_render_face(chunks, block_types, block_number, block_type, (x, y, z - 1)): add_face(5)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x + 1, y, z): add_face(0)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x - 1, y, z): add_face(1)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x, y + 1, z): add_face(2)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x, y - 1, z): add_face(3)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x, y, z + 1): add_face(4)
+						if can_render_face(parent_blocks, chunks, block_types, block_number, block_type, x, y, z - 1): add_face(5)
 					
 					else:
 						for i in range(len(block_type.vertex_positions)):
